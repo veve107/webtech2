1306b0c896b4de7f1af188459cbbf8c7c93ae736
Zolo925 <50198612+Zolo925@users.noreply.github.com>
2019-05-09 11:08:22 +0200

    Add files via upload

---
+++ sk.php
@@ -0,0 +1,31 @@
+<?php
+    $lang = array(
+        "title" => "Finálne zadanie",
+        "home" => "Domov",
+        "zadanie1" => "Zadanie 1",
+        "zadanie2" => "Zadanie 2",
+        "zadanie3" => "Zadanie 3",
+        "text" => "Vytvorte portál pre podporu predmetov “Webové technológie”, ktorý však bude možné použiť aj pre iné predmety.<br>
+        Pre <b>vybraný školský rok a predmet</b> bude možné:<br>
+        &emsp;&emsp;&emsp;1. zobraziť získané body študenta,<br>
+        &emsp;&emsp;&emsp;2. rozdeľovať body medzi členov tímu pri tímových zadaniach,<br>
+        &emsp;&emsp;&emsp;3. generovať heslá pre študentov a rozposlať ich jednotlivým študentom.<br>
+        Po prihlásení študenta cez AIS login sa mu zobrazí vždy iba tá časť, ktorá je pre neho v danom predmete relevantná.<br>
+        Administrátor sa bude prihlasovať do aplikácie vami vygenerovaným heslom.<br>
+        Celá stránka bude dvojjazyčná.<br>
+        Podrobnejší popis úloh nájdete na nasledujúcich stranách.<br>
+        V rámci jednotlivých úloh sa budú do aplikácie importovať údaje vo forme csv súborov. V prípade, že ich budete<br>
+        ukladať do databázy (prvé dve úlohy), tak ich importujte vždy iba do jednej tabuľky slúžiacej pre danú úlohu.<br>
+        Pridanie ďalších stĺpcov do tabuľky ako aj pridanie ďalších tabuliek do databázy je možné. Zabezpečte, že<br>
+        v prípade duplicitného uploadu súboru do databázy sa údaje v databáze aktualizujú.<br>
+        Pri práci na projekte je potrebné používať verzionovací systém, napr. „github“, pričom v technickej dokumentácii<br>
+        musí byť uvedená adresa, kde sa vaše úložisko nachádza.<br>
+        Jednotlivé stránky vytvoreného portálu musia byť responzívne a prispôsobené aj pre tlač !!.<br><br>",
+        "login" => "Prihlásiť sa",
+        "log" => "Prihlásenie do systému",
+        "username" => "Meno",
+        "submit" => "Prihlásiť sa",
+        "password" => "Heslo",
+        "table" => "Názov predmetu"
+    );
+?>
